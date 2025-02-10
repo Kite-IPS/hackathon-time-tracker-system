1d546ef2a6a7416340e43b75d0149852c20c1eb0
Varshini Shree <117148310+VarshiniShreeV@users.noreply.github.com>
2025-02-10 12:19:38 +0530

    Wrote logics and routes

--- app/models.py
+++ app/models.py
@@ -1,23 +1,47 @@
 from . import db
 from datetime import datetime, timezone
 
+total_time = 10800 # Per person 30 hrs
 
 class Team(db.Model):
 
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
 
-    members = db.relationship("student", backref="team")
+    members = db.relationship("Student", backref="Team")
+    
+    @classmethod
+    def team_time_out(self):
+        return sum(member.time_spent_out() for member in self.members if member.time_spent_out is not None)
 
 class Student(db.Model):
-    
     rfid_num = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     roll_num = db.Column(db.String(10), unique=True, nullable=False)
     team_id = db.Column(db.Integer, db.ForeignKey("team._id"))
-
-class Entry(db.Model):
     
+    @classmethod
+    def time_out(cls, rfid_num):
+        entries = Entry.query.filter_by(student=cls.rfid_num).order_by(Entry.timestamp).all()
+        total_time_out = 0
+        last_in_time = None
+        
+        for entry in entries:
+            if entry._id % 2 != 0: # Check In
+                last_in_time = entry.timestamp
+            elif entry._id % 2 == 0 and last_in_time: # Check Out
+                total_time_out += (entry.timestamp - last_in_time).total_seconds() // 60
+                last_in_time = None
+        
+        return total_time_out
+        
+class Entry(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     student = db.Column(db.String(10), db.ForeignKey("student.rfid_num"))
     timestamp = db.Column(db.DateTime, nullable=False, default= lambda: datetime.now(timezone.tzname("Asia/Kolkata")))
+    
+    @classmethod # Do we need this?
+    def in_out(cls, _id, student):
+        return "in" if _id % 2 != 0 else "out"
+    
+    
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .models import *
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def home():
-    return "Success"
+    return render_template('index.html')
+
+@bp.route("/team")
+def team():
+    page = request.args.get('page', 1, type = int)
+    teams = Team.query.all()
+    return render_template('team.html', teams=teams )
+
+@bp.route("/students")
+def student():
+    page = request.args.get('page', 1, type = int)
+    students = Student.query.all() 
+    return render_template('students.html', students=students)
+
+@bp.route("/team-details")
+def teamDetails():
+    page = request.args.get('page', 1, type = int)
+    teams = Team.query.all()
+    return render_template('teamDetails.html', teams= teams)
